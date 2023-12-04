4ab1ff1282a77619d92d4680346113fe5a2b8797
Rubencito2002 <93660491+Rubencito2002@users.noreply.github.com>
2023-12-04 20:38:08 +0100

    modificando las vistas y urls

--- blog/urls.py
+++ blog/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 from . import views
+from .views import post_list,post_detail,post_new, post_edit
 
 urlpatterns = [
-    path('', views.post_list, name='post_list'),
-    path('post/<int:pk>/', views.post_detail, name='post_detail'),
-    path('post/new', views.post_new, name='post_new'),
-    path('post/<int:pk>/edit/', views.post_edit, name='post_edit'),
+    path('', post_list.as_view(), name='post_list'),
+    path('post/<int:pk>/', post_detail.as_view(), name='post_detail'),
+    path('post/new', post_new.as_view(), name='post_new'),
+    path('post/<int:pk>/edit/', post_edit.as_view(), name='post_edit'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,17 +1,54 @@
 from django.shortcuts import get_object_or_404, render
 from django.utils import timezone
 from django.shortcuts import redirect
+from django.urls import reverse_lazy
 from .models import Post
 from .forms import PostForm
+from django.views import View
+from django.views.generic import ListView, DetailView, CreateView, UpdateView
 
+# Vista creada con función 
+"""
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     return render(request, 'blog/post_list.html', {'posts': posts})
+"""
+# Vista creada usando view
+"""
+class post_list(View):
+    template_name = 'blog/post_list.html'
 
+    def get(self, request, *args, **kwargs):
+        posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+        return render(request, self.template_name, {'posts': posts})
+"""
+# Vista creada usando Listview
+class post_list(ListView):
+    model = Post
+    template_name = 'blog/post_list.html'
+    context_object_name = 'posts'
+    ordering = ['published_date']
+
+    def get_queryset(self):
+        return Post.objects.filter(published_date__lte=timezone.now())
+# Vista creada con función 
+"""
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
     return render(request, 'blog/post_detail.html', {'post': post})
+"""
+# Vista creada usando DetailView
+class post_detail(DetailView):
+    model = Post
+    template_name = 'blog/post_detail.html'
+    context_object_name = 'post'
 
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        return context
+
+# Vista creada con función 
+"""
 def post_new(request):
     if request.method == "POST":
         form = PostForm(request.POST)
@@ -24,7 +61,42 @@ def post_new(request):
     else:
         form = PostForm()
     return render(request, 'blog/post_edit.html', {'form': form})
+"""
+# Vista creada usando view
+"""
+class post_new(View):
+    template_name = 'blog/post_edit.html'
+    form_class = PostForm
 
+    def get(self, request, *args, **kwargs):
+        form = self.form_class()
+        return render(request, self.template_name, {'form': form})
+
+    def post(self, request, *args, **kwargs):
+        form = self.form_class(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+            return redirect('post_detail', pk=post.pk)
+        return render(request, self.template_name, {'form': form})
+"""    
+# Vista creada usando UpdateView    
+class post_new(CreateView):
+    model = Post
+    form_class = PostForm
+    template_name = 'blog/post_edit.html'
+    success_url = reverse_lazy('post_list')
+
+    def form_valid(self, form):
+        post = form.save(commit=False)
+        post.author = self.request.user
+        post.published_date = timezone.now()
+        post.save()
+        return super().form_valid(form)
+# Vista creada con función 
+"""
 def post_edit(request, pk):
     post = get_object_or_404(Post, pk=pk)
     if request.method == "POST":
@@ -38,3 +110,42 @@ def post_edit(request, pk):
     else:
         form = PostForm(instance=post)
     return render(request, 'blog/post_edit.html', {'form': form})
+"""
+# Vista creada usando view
+"""
+class post_edit(View):
+    template_name = 'blog/post_edit.html'
+
+    def get(self, request, pk):
+        post = get_object_or_404(Post, pk=pk)
+        form = PostForm(instance=post)
+        return render(request, self.template_name, {'form': form})
+
+    def post(self, request, pk):
+        post = get_object_or_404(Post, pk=pk)
+        form = PostForm(request.POST, instance=post)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+            return redirect('post_detail', pk=post.pk)
+        return render(request, self.template_name, {'form': form})
+"""
+# Vista creada con función 
+class post_edit(UpdateView):
+    model = Post
+    form_class = PostForm
+    template_name = 'blog/post_edit.html'
+    success_url = reverse_lazy('post_detail')
+
+    def form_valid(self, form):
+        post = form.save(commit=False)
+        post.author = self.request.user
+        post.published_date = timezone.now()
+        post.save()
+        return super().form_valid(form)
+
+    def get_object(self, queryset=None):
+        pk = self.kwargs.get('pk')
+        return get_object_or_404(Post, pk=pk)
